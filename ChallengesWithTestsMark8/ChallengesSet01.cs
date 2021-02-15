d876e9ca3e6fdb4b51f7999c3184517d1b21f785
Michael Farris <spacecowby069@yahoo.com>
2021-02-15 12:31:30 -0500

    1st commit

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 return true;
             }
@@ -18,32 +18,49 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int smallestNumber = 0;
+
+            if (number1 < smallestNumber)
+            {
+                smallestNumber = number1;
+            }
+            if (number2 < smallestNumber)
+            {
+                smallestNumber = number2;
+            }
+
+            return smallestNumber;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
-        public string GetGreeting(string nameOfPerson)
+        public string GetGreeting(string nameOfPerson) 
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return ($"Hello, {nameOfPerson}!");
+            //Console.WriteLine($"Hello, {nameOfPerson}!");
         }
-
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "Hey!";
         }
     }
 }
